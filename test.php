0717381515ea536bb01419ea9f65dff17c3f7c0c
noahmaru5 <sm50b2140@outlook.jp>
2019-08-21 23:38:28 +0900

    passwordなど気を付けてね

--- test.php
+++ test.php
@@ -1,17 +1,17 @@
 <?php
-    $s=new PDO("mysql:host=localhost;dbname=schedb","root","noaH0925wapli");
+    $s=new PDO("mysql:host=localhost;dbname=","root","");
     print "接続OK<br>";
 
-    $re=$s->query("SELECT lec FROM kleccansel");
+    $re=$s->query("SELECT * FROM ");
     while($kekka=$re->fetch()){
         print $kekka[0];
-        //print ":";
-        //print $kekka[1];
-        //print"\t";
-        //print ":";
-        //print $kekka[2];
-        //print ":";
-        //print $kekka[3];*/
+        print ":";
+        print $kekka[1];
+        print"\t";
+        print ":";
+        print $kekka[2];
+        print ":";
+        print $kekka[3];
         print "<br>";
         
     }
